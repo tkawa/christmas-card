35a967ebe01bd53f2222c3eb26d4288c55858e63
Toru KAWAMURA <tkawa@4bit.net>
2012-12-01 15:06:28 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChristmasCard::Application.config.secret_token = '10c29b68cf8bd272144c28b93c315f6460723befd621119465e09c6d0f3a7a4c8e33b3cfb09ab0bd92edef9799df1195a001774d487ae2db1f5fbd3a38eb8541'
