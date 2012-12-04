4b9e766fac7f0572c0e11a1538397cf4d5fc3b32
Toru KAWAMURA <tkawa@4bit.net>
2012-12-05 02:30:48 +0900

    Add member's image

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -4,14 +4,16 @@ FactoryGirl.define do
   factory :ppworks, class: Member do
     id 1
     name "Koshikawa"
-    #image_url "MyString"
+    image_url "https://si0.twimg.com/profile_images/2900491556/9d2bf873770958647f18b8e61af31f1a_normal.png"
   end
   factory :fukajun, class: Member do
     id 2
     name "Fukaya"
+    image_url "https://si0.twimg.com/profile_images/2858628753/5642fbccdb7db9a55ebe66aa789c060f_normal.jpeg"
   end
   factory :tkawa, class: Member do
     id 3
     name "Kawamura"
+    image_url "https://si0.twimg.com/profile_images/69862547/07280001_normal.jpg"
   end
 end
