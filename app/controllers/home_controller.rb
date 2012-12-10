648c2161baa5725f2fb29abd81402d3e61b57b3d
Toru KAWAMURA <tkawa@4bit.net>
2012-12-11 08:44:56 +0900

    Redirect root to Listing destinations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    redirect_to destinations_path
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe HomeController do
-  before do
-    auth_admin
-  end
-
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    context 'with auth' do
+      before do
+        auth_admin
+      end
+
+      it "returns http success" do
+        get 'index'
+        response.should redirect_to destinations_path
+      end
+    end
+
+    context 'without auth' do
+      it "requires auth" do
+        get 'index'
+        response.status.should be 401
+      end
     end
   end
 
